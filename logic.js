c89b189aef5d621661b0da52cb7da59b6c255870
Emiliano <emilianosteinvorth@gmail.com>
2023-04-12 16:48:33 -0600

    Added working js to html.

---
+++ logic.js
@@ -0,0 +1,11 @@
+let rock = "Rock";
+let paper = "Paper";
+let scissors = "Scissors";
+
+function getComputerChoice(){
+    let arrayMoves = {rock, paper, scissors};
+
+    console.log(arrayMoves);
+}
+
+getComputerChoice();
