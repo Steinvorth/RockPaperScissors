8432928114e2c0cc1d017a2fd3bc35fc0ffaa9d9
Emiliano <emilianosteinvorth@gmail.com>
2023-04-12 17:42:37 -0600

    Changes to player logic, not working propperly

--- logic.js
+++ logic.js
@@ -1,12 +1,21 @@
 function getComputerChoice(arr){
-    
-
     const compMoves = Math.floor(Math.random()* arrayMoves.length);
 
     const selMove = arr[compMoves];
     return selMove;
 }
-const arrayMoves = ["rock", "paper", "scissors"];
 
-const compSelMove = getComputerChoice(arrayMoves);
-console.log(compSelMove);
+function playRound(playerSelection, computerSelection){  
+    return playerSelection,computerSelection;
+}
+
+function game(){
+
+}
+
+
+//move array
+const arrayMoves = ["rock", "paper", "scissors"];
+const computerSelection = getComputerChoice(arrayMoves);
+const playerSelection = 'rock';
+console.log(playRound(playerSelection, computerSelection));
