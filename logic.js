f1d2e629802f44ebb2b8a90c47308a1c5d6bb0fe
Emiliano <emilianosteinvorth@gmail.com>
2023-04-12 18:05:01 -0600

    Added working logic to round, added game func, not completely working

--- logic.js
+++ logic.js
@@ -6,16 +6,42 @@ function getComputerChoice(arr){
 }
 
 function playRound(playerSelection, computerSelection){  
-    return playerSelection,computerSelection;
+    let winner = "";
+
+    let playerInput = prompt();
+
+    playerSelection = playerInput;
+
+    if(computerSelection === playerSelection){
+        winner = `Tie! your opponent also chose ${computerSelection}`;
+    }
+    else if(computerSelection === "rock" & playerInput === "scissors"){
+        winner = `You loose! ${computerSelection} beats ${playerInput}`
+    }
+    else if(computerSelection === "paper" & playerInput === "rock"){
+        winner = `You loose! ${computerSelection} beats ${playerInput}`
+    }
+    else if(computerSelection === "scissors" & playerInput === "paper"){
+        winner = `You loose! ${computerSelection} beats ${playerInput}`
+    }
+    else{
+        winner = `You Win! ${playerInput} beats ${computerSelection}`
+    }
+
+    return playerSelection,computerSelection, winner;
 }
 
 function game(){
-
+    let playerScore= 0;
+    let oppSocre = 0;
+    for(i = 0; i<5; i++){        
+        console.log(playRound(playerSelection,computerSelection));
+    }
 }
 
 
 //move array
 const arrayMoves = ["rock", "paper", "scissors"];
 const computerSelection = getComputerChoice(arrayMoves);
-const playerSelection = 'rock';
-console.log(playRound(playerSelection, computerSelection));
+const playerSelection = "";
+console.log(game());
