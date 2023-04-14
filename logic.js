132c2ebc1bd8ea6a75f4ea2a8874eaf72f9e8ef0
Emiliano <emilianosteinvorth@gmail.com>
2023-04-14 11:14:56 -0600

    Game works, all logic is done, Scores and winner working.

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+//Basic Opponent
 function getComputerChoice(arr){
     const compMoves = Math.floor(Math.random()* arrayMoves.length);
 
@@ -5,57 +6,76 @@ function getComputerChoice(arr){
     return selMove;
 }
 
+// logic to play game, choose move, determine score and winner message each round
 function playRound(playerSelection, computerSelection){  
 
     let playerInput = prompt().toLowerCase();
-    let playerScore = false;
 
+    //makes player input work.
     playerSelection = playerInput;
 
+    //tie
     if(computerSelection === playerSelection){
         winner = `Tie! your opponent also chose ${computerSelection}`;
     }
+    //Computer wins
     else if(computerSelection === "rock" & playerInput === "scissors"){
         winner = `You loose! ${computerSelection} beats ${playerInput}`
-        playerWin = false;
+        opponentWin = opponentWin + 1;
+
     }
     else if(computerSelection === "paper" & playerInput === "rock"){
         winner = `You loose! ${computerSelection} beats ${playerInput}`
-        playerWin = false;
+        opponentWin = opponentWin + 1;
     }
     else if(computerSelection === "scissors" & playerInput === "paper"){
         winner = `You loose! ${computerSelection} beats ${playerInput}`
-        playerWin = false;
+        opponentWin = opponentWin + 1;
     }
+    //Stopps player from inputting random words
     else if(arrayMoves.indexOf(playerInput.toLowerCase()) === -1){
         winner = `You can't choose ${playerInput}!`
     }
+    //Player win
     else{
         winner = `You Win! ${playerInput} beats ${computerSelection}`
-        playerWin = true;
+        playerWin = playerWin+1;
     }
     
 
     return playerSelection,computerSelection, winner;
 }
 
+//Logic to start game, and announce winner at the end
 function game(){
+    //for loop for 5 matches in a row.
     for(i = 0; i<5; i++){        
-        console.log(playRound(playerSelection,computerSelection));
+        console.log(playRound(playerSelection,computerSelection))
+        console.log(`Player: ${playerWin} vs Opponent: :${opponentWin}`);        
+    }   
+
+    //Announce winner at end of match
+    if(playerWin > opponentWin){
+        console.log("You are the winner!!")
     }
-    //console.log(`Scores: ${playerScore} : ${oppSocre}`)
-    
+    else if(playerWin === opponentWin){
+        console.log("There has been a tie!");
+    }
+    else{
+        console.log("Your Opponent Wins!!")
+    }
 }
 
 
-//move array
+//Computer move array, olayer check to not use random words
 const arrayMoves = ["rock", "paper", "scissors"];
 const computerSelection = getComputerChoice(arrayMoves);
 let playerSelection = "";
 
-//Score Vars?
+//Scores
 let winner = "";
-let playerWin;
+let playerWin = 0;
+let opponentWin = 0;
 
 //init game
 console.log(game());
