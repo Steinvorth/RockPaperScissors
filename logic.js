5e6cb0a6b481dd8442f5e5fdcff8a2ac0bf455f6
Emiliano <emilianosteinvorth@gmail.com>
2023-04-23 20:02:55 -0600

    Changes to attempt to implement button player logic

--- logic.js
+++ logic.js
@@ -4,36 +4,19 @@ const rockMove = document.querySelector('#rock');
 const paperMove = document.querySelector('#paper');
 const scissorMove = document.querySelector('#scissors');
 
-//Event Listeners for Buttons
-startGame.addEventListener('click', function(playGame){
-    console.log(game());
-});
-
-rockMove.addEventListener('click', function(choseRock){
-    console.log(arrayMoves[0]);
-});
-
-paperMove.addEventListener('click', function(chosePaper){
-    console.log(arrayMoves[1]);
-});
-
-scissorMove.addEventListener('click', function(choseScissor){
-    console.log(arrayMoves[2]);
-});
-
 //Winner and Scoreboard DOM
 
 //Basic Opponent
 function getComputerChoice(arr){
     const compMoves = Math.floor(Math.random()* arrayMoves.length);
 
-    const selMove = arr[compMoves];
-    return selMove;
+    const selectedMove = arr[compMoves];
+    return selectedMove;
 }
 
 // logic to play game, choose move, determine score and winner message each round
 function playRound(playerSelection, computerSelection){  
-
+    
     let playerInput;
 
     //makes player input work.
@@ -57,10 +40,6 @@ function playRound(playerSelection, computerSelection){
         winner = `You loose! ${computerSelection} beats ${playerInput}`
         opponentWin = opponentWin + 1;
     }
-    //Stopps player from inputting random words
-    else if(arrayMoves.indexOf(playerInput.toLowerCase()) === -1){
-        winner = `You can't choose ${playerInput}!`
-    }
     //Player win
     else{
         winner = `You Win! ${playerInput} beats ${computerSelection}`
@@ -74,10 +53,10 @@ function playRound(playerSelection, computerSelection){
 //Logic to start game, and announce winner at the end
 function game(){
     //for loop for 5 matches in a row.
-    // for(i = 0; i<5; i++){        
-    //     console.log(playRound(playerSelection,computerSelection))
-    //     console.log(`Player: ${playerWin} vs Opponent: :${opponentWin}`);        
-    // }   
+    for(i = 0; i<5; i++){        
+        console.log(playRound(playerSelection,computerSelection))
+        console.log(`Player: ${playerWin} vs Opponent: :${opponentWin}`);        
+    }   
 
     //Announce winner at end of match
     if(playerWin > opponentWin){
@@ -102,9 +81,24 @@ let winner = "";
 let playerWin = 0;
 let opponentWin = 0;
 
-let playerSelection = '';
+let playerSelection;
 
+// //Event Listeners for Buttons
+startGame.addEventListener('click', function(playGame){
+    console.log(game());
+});
 
+rockMove.addEventListener('click', function(choseRock){
+    console.log('rock');
+});
+
+paperMove.addEventListener('click', function(chosePaper){
+    console.log('paper');
+});
+
+scissorMove.addEventListener('click', function(choseScissor){
+    console.log('scissor');
+});
 
 //init game
-//console.log(game());
+console.log(playRound());
